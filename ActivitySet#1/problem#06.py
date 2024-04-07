749323172a8a4b02e1dce082bf53b0897a5b24b3
Sanvi-Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2024-04-07 20:08:39 +0530

    Update problem#06.py

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,16 +1,18 @@
-# Loops & Iterators
-
 largest = None
-smallest = None
-
+smallest = None  
 while True:
-    num = input("Enter a number? ")
-
+    num = input("Enter a number: ")
     if num == "done":
         break
+    try:
+        num_ip=int(num)
+        if largest is None or num_ip > largest:
+              largest = num_ip
+        elif smallest is None or num_ip < smallest:
+              smallest = num_ip
+    except:
+        print("Invalid input")
+        continue
 
-    # ...
-
-    print(num)
-
-print("Maximum", largest)
+print("Maximum is", largest)
+print("Minimum is", smallest)
